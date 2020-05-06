ec41c7ffc7d5127e027f50d7fc8c33390c40bba5
Julien Mastrangelo <mastrangelo.julien@gmail.com>
2020-05-06 16:07:28 +0200

    contact.php var

--- contact.php
+++ contact.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_POST['name']))	{$name = $_POST['name'];}
+if (isset($_POST['email']))	{$email = $_POST['email'];}
+if (isset($_POST['phone']))	{$phone = $_POST['phone'];}
+if (isset($_POST['message']))	{$message = $_POST['message'];}
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
