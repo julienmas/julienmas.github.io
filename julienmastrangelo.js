eab2c8bddb42cd5aaacea7ec070205ee750152d9
Julien Mastrangelo <mastrangelo.julien@gmail.com>
2020-07-22 16:52:18 +0200

    add files

---
+++ julienmastrangelo.js
@@ -0,0 +1,16 @@
+var moreText = document.querySelectorAll('.more');
+var btnText = document.querySelectorAll('.btn');
+
+function readMore(i)
+{
+  if (moreText[i].style.display === 'inline')
+  {
+    btnText[i].innerHTML = 'Read more';
+    moreText[i].style.display = 'none';
+  }
+  else
+  {
+    btnText[i].innerHTML = 'Read less';
+    moreText[i].style.display = 'inline';
+  }
+}
